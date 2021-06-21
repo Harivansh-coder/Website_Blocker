8be522066ee048eae0a896be7dddd0e77f99412c
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2021-06-21 11:52:16 +0530

    Funtion call loop removed

--- Website Blocker/websiteBlocker.py
+++ Website Blocker/websiteBlocker.py
@@ -1,39 +1,45 @@
-# The following code is for linux/mac
+#Website Blocker without timer, funtion call oriented
 
-from datetime import datetime ,timedelta
+import platform
 
-current = datetime.now()
+os_name = platform.system()
 
-#amount of time for the website to be blocked
-time = 1 # if you want to change amount of blocked time change value here (in Hours)
+sites = ["youtube.com","www.youtube.com","facebook.com","www.facebook.com","reddit.com"]
 
-end = current + timedelta(hours=time) #time till the websites will be blocked
-
-sites = ["www.youtube.com","youtube.com","www.twiter.com","www.facebook.com"]
-
-hostfile_path = "/etc/hosts"
+if os_name == "Linux" or os_name == "Darwin":
+    hostfile_path = "/etc/hosts"
+elif os_name == "Windows":
+    hostfile_path = "C:/Windows/System32/drivers/etc/hosts"
 
 
 local = "127.0.0.1"
 
+
 def block():
-    if current < end:
-        print("Sites Blocked")
-        with open(hostfile_path,'r+') as hostfile:
-            file_content = hostfile.read()
-            for site in sites:
-                if site not in file_content:
-                    hostfile.write(local+" "+site+"\n")
-    else:
-        print("Sites Unblocked")
-        with open(hostfile_path,'r+') as hostfile:
-            lines = hostfile.readline
-            hostfile.seek(0)
-            for line in lines:
-                if  not any(site in line for site in sites):
-                    hostfile.write(line)
-            hostfile.truncate()
+    
+    print("Blocked sites")
+    with open(hostfile_path,'r+') as hostfile:
+        file_content = hostfile.read()
+        for site in sites:
+            if site not in file_content:
+                hostfile.write(local+" "+site+"\n")
+
+def unBlock():
+    print("sites unblocked")
+    with open(hostfile_path,'r+') as hostfile:
+        lines = hostfile.readline()
+        hostfile.seek(0)
+        for line in lines:
+            if  not any(site in line for site in sites):
+                hostfile.write(line)
+        hostfile.truncate()
+
+        
 
 if __name__ == "__main__":
-    block()
+
+    block()#To block
+    unBlock()#To Unblock
+
+
 
